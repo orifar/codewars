79ef2b73d3cf0264507ba4b14d39b49259540fa2
rifar <rifar317@gmail.com>
2022-03-18 17:01:19 +0100

    init 2

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from test_tools import *
 from solved import *
+
+
 # def to_jaden_case(string):
 #     string = string.replace('\'','')
 #     string = string.title()
 #     string = string.replace('Arent', 'Aren\'t')
 #     return string
 
-def buddy(start, limit):
-    def divisor(num):
-        divisors = []
-        i = 1
-        while i < num:
-            if num % i:
-                divisors.append(i)
-                i+=1
-        return divisors
+def divisor(num):
+    l = []
+    i = 1
+    while i < num:
+        if (num % i==0):
+            l.append(i)
+            i += 1
+        else: i += 1
+    return l
 
-    if sum(a for a in divisor(strat)) == 1+ sum(b for b in divisor(limit)) or sum(b for b in divisor(limit)) == 1+ sum(a for a in divisor(strat)):
-        return ()
+print(divisor(48))
+print(divisor(75))
+
+
+# if sum(a for a in divisor(strat)) == 1+ sum(b for b in divisor(limit)) or sum(b for b in divisor(limit)) == 1+ sum(a for a in divisor(strat)):
+#     return ()
